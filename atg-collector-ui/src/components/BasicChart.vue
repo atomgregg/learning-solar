01a546ac70275fc4b3af0d025dfc338ec61ad107
Aaron Gregg <atomgregg@outlook.com>
2023-06-09 10:04:39 +0200

    added buttons to select different default time ranges

--- atg-collector-ui/src/components/BasicChart.vue
+++ atg-collector-ui/src/components/BasicChart.vue
@@ -1,76 +1,119 @@
 <template>
+  <div class="button-container">
+    <button :class="{ active: selectedOption === 'today' }" @click="getData('today')">Today</button>
+    <button :class="{ active: selectedOption === 'yesterday' }" @click="getData('yesterday')">Yesterday</button>
+    <button :class="{ active: selectedOption === 'recent' }" @click="getData('recent')">Recent</button>
+    <button :class="{ active: selectedOption === 'trend' }" @click="getData('trend')">Trend</button>
+  </div>
+
   <div>
     <Line
       v-if="loaded"
       :options="chartOptions"
       :data="chartData"
       :width="400"
-      :height="800" />
+      :height="800"
+    />
   </div>
 </template>
 
 <script>
-import { Line } from 'vue-chartjs'
-import axios from 'axios';
+import { Line } from 'vue-chartjs';
+import { ref, onMounted } from 'vue';
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js'
+import axios from 'axios';
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement);
 
 export default {
-  name: 'LineChart',
+  name: 'BasicChart',
   components: {
-    Line
+    Line,
   },
-  data: () => ({
-    loaded: false,
-    chartData: null,
-    chartOptions: {
+  setup() {
+    const loaded = ref(false);
+    const chartData = ref({});
+    const chartOptions = {
       responsive: true,
-      maintainAspectRatio: false
-    }
-  }),
-  async mounted () {
-    this.loaded = false;
+      maintainAspectRatio: false,
+    };
+    const selectedOption = ref('yesterday');
+    const colors = ['#007bff', '#28a745', '#ffc107', '#dc3545'];
 
-    var colors = ['green', 'blue', 'yellow'];
+    const getData = async (option) => {
+      try {
+        const response = await axios.get(`https://localhost:7251/api/data/${option}`);
+        const data = response.data;
 
-    try {
-      axios
-        .get('https://localhost:7251/api/data/recent')
-        .then((response) => {
-          const labels = [...new Set(response.data.map((item) => item.tstamp))];
-          const datasets = [];
+        const labels = [...new Set(data.map((item) => item.tstamp))];
+        const datasets = [];
 
-          // group data by rowKey
-          const groupedData = response.data.reduce((acc, item, idx) => {
-            if (!acc[item.rowKey]) {
-              acc[item.rowKey] = {
-                label: item.rowKey,
-                data: [],
-                borderWidth: 1,
-                fill: false,
-                borderColor: colors[idx]
-              };
-            }
-            acc[item.rowKey].data.push(item.intValue);
-            return acc;
-          }, {});
-
-          // transform grouped data to datasets format
-          for (const key in groupedData) {
-            datasets.push(groupedData[key]);
+        const groupedData = data.reduce((acc, item, idx) => {
+          if (!acc[item.rowKey]) {
+            acc[item.rowKey] = {
+              label: item.rowKey,
+              data: [],
+              borderWidth: 1,
+              fill: false,
+              borderColor: colors[idx % colors.length],
+            };
           }
-          
-          // set the data
-          this.chartData = {labels, datasets };
-          this.loaded = true;
-      })
-      .catch((error) => {
+          acc[item.rowKey].data.push(item.intValue);
+          return acc;
+        }, {});
+
+        for (const key in groupedData) {
+          datasets.push(groupedData[key]);
+        }
+
+        chartData.value = {
+          labels,
+          datasets,
+        };
+        loaded.value = true;
+      } catch (error) {
         console.error(error);
-      });
-    } catch (e) {
-      console.error(e)
-    }
-  }
-  };
+      }
+    };
+
+    onMounted(async () => {
+      try {
+        await getData('yesterday');
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    return {
+      loaded,
+      chartData,
+      chartOptions,
+      getData,
+      selectedOption,
+    };
+  },
+};
 </script>
+
+<style>
+.active {
+  background-color: #007bff;
+  color: #fff;
+}
+.button-container {
+  margin-top: 10px;
+}
+.button-container button {
+  margin-right: 5px;
+  border: none;
+  border-radius: 4px;
+  padding: 8px 12px;
+  background-color: #f7f7f7;
+  color: #333;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+.button-container button:hover {
+  background-color: #e9e9e9;
+}
+</style>
